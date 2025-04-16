7eaee97b76f50f7cffdc4aaaac74d2691932e9ee
WaverDaver <109171547+WaverDaver@users.noreply.github.com>
2025-04-16 13:35:38 -0400

    colors and fancy blob

--- book-o-mend/src/App.jsx
+++ book-o-mend/src/App.jsx
@@ -9,9 +9,15 @@ function App(){
 
   return(
     <>
-    <div>
+    <div class="relative h-screen bg-white overflow-hidden">
+  <svg class="absolute top-0 left-0 w-full h-full z-0" viewBox="0 0 800 400" fill="none" preserveAspectRatio='none'> 
+    <path fill="#ffb74d" d="M0,100 C200,200 600,0 800,100 L800,400 L0,400 Z" />
+  </svg>
+  <div className='relative z-10'>
   <MainHeader></MainHeader>
   <MainPage></MainPage>
+  </div>
+  
 
     </div>
 
--- book-o-mend/src/components/MainPage.jsx
+++ book-o-mend/src/components/MainPage.jsx
@@ -6,7 +6,7 @@ function MainPage(){
         
         <div className="flex flex-col justify-center items-center w-screen mt-50">
             <h1 className="font-extrabold text-7xl">
-                <span>How readers find their next </span> 
+                <span className="">How readers find their next </span> 
                 <span className="text-orange-400">favorite book</span>
             </h1>
             <button className=" px-8 py-4 mt-3.5 shadow-sm hover:shadow-lg transition duration-300 text-1xl text-black font-semibold rounded-lg 
